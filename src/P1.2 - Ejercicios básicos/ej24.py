4eaad15353d865e063d9f2b40284df65eec7faeb
mtamrod <trmarioiesrafaelalberti.es>
2024-10-02 22:59:25 +0200

    Ajuste de ej24 y subida de ej25

--- src/P1.2 - Ejercicios básicos/ej24.py
+++ src/P1.2 - Ejercicios básicos/ej24.py
@@ -1,9 +1,5 @@
 precio_producto = input("Introduce el precio del producto con 2 decimales: ")
 
-lista_precio = []
-pos_producto = precio_producto.find(".")
-lista_precio.append(precio_producto[0:pos_producto])
-lista_precio.append(precio_producto[pos_producto+1:])
-
-print(f"Son {lista_precio[0]} euros y {lista_precio[1]} centimos")
+precio_producto = precio_producto.split(".")
+print(f"Son {precio_producto[0]} euros y {precio_producto[1]} centimos")
 
