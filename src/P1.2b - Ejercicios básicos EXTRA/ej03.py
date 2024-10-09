1664e621060a4e5c2798465d3879be89f8086b0a
mtamrod <trmarioiesrafaelalberti.es>
2024-10-09 13:48:22 +0200

    Ejercicios Extra

--- src/P1.2b - Ejercicios básicos EXTRA/ej03.py
+++ src/P1.2b - Ejercicios básicos EXTRA/ej03.py
@@ -4,35 +4,35 @@ alto = 12.0
 #1.
 resultado = 0
 resultado = ancho / alto
-print(f"{resultado}")
+print(f"ancho / alto = {resultado} y es de tipo {type(resultado)}")
 
 #2.
 resultado = 0
 resultado = ancho // 2
-print(f"{resultado}")
+print(f"ancho // 2 = {resultado} y es de tipo {type(resultado)}")
 
 #3.
 resultado = 0
 resultado = alto / 2
-print(f"{resultado}")
+print(f"alto / 2 = {resultado} y es de tipo {type(resultado)}")
 
 #4.
 resultado = 0
 resultado = ancho * 2
-print(f"{resultado}")
+print(f"ancho * 2 = {resultado} y es de tipo {type(resultado)}")
 
 #5.
 resultado = 0
 resultado = ancho * alto
-print(f"{resultado}")
+print(f"ancho * alto = {resultado} y es de tipo {type(resultado)}")
 
 #6.
 resultado = 0
 resultado = (5 + 1) * 3
-print(f"{resultado}")
+print(f"(5 + 1) * 3 = {resultado} y es de tipo {type(resultado)}")
 
 #7.
 resultado = 0
 resultado = (5 + 1) / 3
-print(f"{resultado}")
+print(f"(5 + 1) / 3 = {resultado} y es de tipo {type(resultado)}")
 
