ca81a4867ec75c75c100836fd80491292e1d125c
mtamrod534 <trmario@iesrafaelalberti.es>
2024-10-09 17:33:43 +0200

    Subida de ejercicios

---
+++ src/P1.2c - Soluciona estos problemas/ej29.py
@@ -0,0 +1,5 @@
+"""Realiza un programa en Python que solicite un nombre y una edad.
+
+Si el nombre está vacío, debes utilizar el valor "Desconocido". La edad debe pedirse hasta que introduzca una edad comprendida entre 0 y 125 años.
+El programa mostrará la siguiente frase: "Te llamas Pepito y tienes 20 años, te quedan aún 105 años por cumplir"."""
+
